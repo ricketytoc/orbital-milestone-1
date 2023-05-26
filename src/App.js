fb6267790aa417fbae7cf6927bec0c5342c698d0
richard <122388382+ricketytoc@users.noreply.github.com>
2023-05-26 08:00:35 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello RIchard CodeSandbox</h1>
+      <h1>Hello RIchard Hew CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
